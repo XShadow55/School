05345a633623827310ca5dac48954d1345b4d5a4
XShadow55 <127655772+XShadow55@users.noreply.github.com>
2024-03-13 16:08:44 +0300

    Update InfoController.java

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -3,7 +3,7 @@ package ru.hogwarts.school.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+// получение порта использованного при запуске приложения
 @RestController
 public class InfoController {
     @Value("${server.port}")
