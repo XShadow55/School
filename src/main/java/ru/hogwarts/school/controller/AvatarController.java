4d939b761a8a531587e2daaa53f0ff942d86f9b5
XShadow55 <127655772+XShadow55@users.noreply.github.com>
2024-03-13 15:40:11 +0300

    Update AvatarController.java

--- src/main/java/ru/hogwarts/school/controller/AvatarController.java
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -25,12 +25,13 @@ public class AvatarController {
     public AvatarController(AvatarService avatarServis) {
         this.avatarServis = avatarServis;
     }
-
+    
     @PostMapping(value = "/{studentId}/avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> uploadAvatar(@PathVariable Long studentId, @RequestParam MultipartFile avatar) throws IOException {
         avatarServis.uploadAvatar(studentId, avatar);
         return ResponseEntity.ok().build();
     }
+    
     @GetMapping(value = "/{id}/avatar-from-db")
     public ResponseEntity<byte[]> downloadAvatar(@PathVariable Long id) {
         Avatar avatar = avatarServis.findAvatar(id);
@@ -39,6 +40,7 @@ public class AvatarController {
         headers.setContentLength(avatar.getData().length);
         return ResponseEntity.status(HttpStatus.OK).headers(headers).body(avatar.getData());
     }
+    
     @GetMapping(value = "/{id}/avatar-from-file")
     public void downloadAvatar(@PathVariable Long id, HttpServletResponse response) throws IOException{
         Avatar avatar = avatarServis.findAvatar(id);
@@ -51,15 +53,12 @@ public class AvatarController {
             is.transferTo(os);
         }
     }
+    
     @GetMapping()
     public ResponseEntity<List<Avatar>>findAllAvatars(@RequestParam("page") Integer pageNumber, @RequestParam("size") Integer pageSiza) throws IOException {
         List<Avatar> list = avatarServis.findAll(pageNumber, pageSiza);
         return ResponseEntity.ok(list);
     }
-    @GetMapping("/sum")
-    public long calculateSum() {
-        long n = 1_000_000;
-        long sum = n * (n + 1) / 2;
-        return sum;
-    }
+    
+    
 }
