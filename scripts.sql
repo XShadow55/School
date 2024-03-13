b693acb8b11116ab5d2dcc617d2b20255672975f
XShadow55 <127655772+XShadow55@users.noreply.github.com>
2024-03-13 15:29:26 +0300

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -1,11 +1,15 @@
+// Запрос в бд для получения имён студентов
 select * from student
 select name from student
+// Запрос в бд для получения студентов возрост которых больше 10 и меньше 30
 select * from student
 where age > 10 and age < 30
+// Запрос в бд для получения студентов возрост которых меньше их id
 select * from student
 where age < id
+// Запрос в бд для получения студентов в имени которых есть буква о
 select * from student
 where name like '%о%'
-
+// Запрос в бд для сортировки студентов по возросту
 select * from student
 order by age
