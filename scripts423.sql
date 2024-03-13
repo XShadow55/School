4a4fb838790f9680599ece5a97ed23febb43c6d9
XShadow55 <127655772+XShadow55@users.noreply.github.com>
2024-03-13 15:38:40 +0300

    Update scripts423.sql

--- scripts423.sql
+++ scripts423.sql
@@ -1,3 +1,4 @@
+// Объединение по столбцу faculty_id имени, возроста студента и названия факультета
 select s."name",s."age",f."name" from student s join faculty f on (s.faculty_id = f."id");
-
+// Объединение по столбцу id студента и его аватара
 select * from student s join avatar a on (s.id = a.student_id);
