3b8c9e375fc9de2b6116bd75358abfb1194fc793
XShadow55 <127655772+XShadow55@users.noreply.github.com>
2024-03-13 16:03:48 +0300

    Update StudentServiceImpl.java

--- src/main/java/ru/hogwarts/school/service/impl/StudentServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/impl/StudentServiceImpl.java
@@ -21,11 +21,7 @@ public class StudentServiceImpl implements StudentService {
     public StudentServiceImpl(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
-
-
-
-
-
+// Was invoked method for create student
     @Override
     public Student add(String name, Integer age) {
         logger.info("Was invoked method for create student");
@@ -33,68 +29,70 @@ public class StudentServiceImpl implements StudentService {
         studentRepository.save(student);
         return student;
     }
-
+// The method for reading students was called
     @Override
     public Student read(Long id) {
         logger.info("The method for reading students was called");
         return studentRepository.findById(id).get();
     }
-
+// A method was called to change the student
     @Override
     public Student set(Student student) {
         logger.info("A method was called to change the student");
         return studentRepository.save(student);
     }
-
+// A method was called to remove a student
     @Override
     public void remove(Long id) {
         logger.info("A method was called to remove a student");
         studentRepository.deleteById(id);
     }
+    // A method was called to filter students
     public Collection<Student> filter(int age) {
         logger.info("A method was called to filter students");
         return studentRepository.findByAge(age);
     }
-
+// A method was called to filter students
     @Override
     public Collection<Student> filterByAgeBetween(int min, int max) {
         logger.info("A method was called to filter students");
         return studentRepository.findByAgeBetween(min,max);
     }
-
+// A method was called to filter by student age
     @Override
     public Faculty getFaculty(Long id) {
         logger.info("A method was called to filter by student age");
         return studentRepository.findById(id).map(Student::getFaculty).orElse(null);
     }
-
-
+// A method was called to count the number of students
     @Override
     public Integer countStudent() {
         logger.info("A method was called to count the number of students");
         return studentRepository.countStudent();
     }
+    // A method was called to find the average age of students
     @Override
     public Integer averageAge() {
         logger.info("A method was called to find the average age of students");
         return studentRepository.averageAge();
     }
+    // A method was called to call the last students
     @Override
     public List<Student> lastedStudent() {
         logger.info("A method was called to call the last students");
         return studentRepository.lastedStudent();
     }
-
+// Сортировка студентов по первой букве имени как русских так и английских
     @Override
     public List<Student> listStudent() {
         return studentRepository.findAll().stream().filter(student -> student.getName().toLowerCase().toCharArray()[0]=='a' || student.getName().toLowerCase().toCharArray()[0]=='а' ).toList();
     }
-
+// нахождение среднего возроста студентов
     @Override
     public Double averageAgeStudent() {
         return studentRepository.findAll().stream().mapToInt(Student::getAge).average().orElse(0);
     }
-
+// получение всех студентов 
     @Override
     public List<Student> allStudent() {
         return studentRepository.findAll();
