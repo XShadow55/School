7bc34480c97c105802d60bf2be252a5613bce80e
XShadow55 <127655772+XShadow55@users.noreply.github.com>
2024-03-13 16:00:06 +0300

    Update FacultyServiceImpl.java

--- src/main/java/ru/hogwarts/school/service/impl/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/impl/FacultyServiceImpl.java
@@ -21,47 +21,48 @@ public class FacultyServiceImpl implements FacultyService {
     }
 
     private long count = 0;
-
+// Was invoked method for create faculty
     @Override
     public Faculty add(Faculty faculty) {
         logger.info("Was invoked method for create faculty");
             return facultyRepository.save(faculty);
     }
-
+// The method for reading faculties was called
     @Override
     public Faculty read(Long id) {
         logger.info("The method for reading faculties was called");
         return facultyRepository.findById(id).get();
     }
-
+// A method was called to change the faculty
     @Override
     public Faculty set(Faculty faculty) {
         logger.info("A method was called to change the faculty");
         return facultyRepository.save(faculty);
     }
-
+// A method was called to remove a faculty
     @Override
     public void remove(Long id) {
         logger.info("A method was called to remove a faculty");
         facultyRepository.deleteById(id);
     }
+    // "A method was called to filter faculties
     public Collection<Faculty> filter(String color) {
         logger.info("A method was called to filter faculties");
         return facultyRepository.findByColor(color);
     }
-
+// A method was called to search for a faculty by name and color, ignoring case
     @Override
     public Collection<Faculty> findByNameIgnoreCaseOrColorIgnoreCase(String color, String name) {
         logger.info("A method was called to search for a faculty by name and color, ignoring case");
         return facultyRepository.findByNameIgnoreCaseOrColorIgnoreCase(color,name);
     }
-
+// A method was called to get students belonging to this faculty
     @Override
     public List<Student> getStudents(Long id) {
         logger.info("A method was called to get students belonging to this faculty");
         return facultyRepository.findById(id).map(Faculty::getStudents).orElse(null);
     }
-
+// Получение самого длинного названия факультета
     @Override
     public String    lengthFaculty() {
         return facultyRepository.findAll().stream().sorted(Comparator.comparing(Faculty::getName).reversed()).toList().get(0).getName();
