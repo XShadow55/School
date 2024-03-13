7179c686e30de0e68f1ae5b40aa0e88a2265c080
XShadow55 <127655772+XShadow55@users.noreply.github.com>
2024-03-13 15:34:53 +0300

    Update scripts421.sql

--- scripts421.sql
+++ scripts421.sql
@@ -1,9 +1,10 @@
+// добавление условия для студентов 
 alter table student add constraint age_check check (age >= 16);
-
+// добавление уникальных имен студентов
 alter table student add constraint name_unique unique (name);
-
+// добавление уникальных имен студентов
 alter table faculty add constraint faculty_unique unique (name,color);
-
+// добавление уникальных имен и цвета факультета
 alter table student alter column name set not null;
-
+// задание условия возроста если он не указан
 alter table student alter column age set default 20;
